50f63ac3e87692452a261a6973975abc3b7cdce2
Joshua Reed <reedjosh@oregonstate.edu>
2015-10-30 11:30:42 -0700

    SPI and debounce moved to ISR

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -9,28 +9,24 @@
 #include <avr/cpufunc.h>
 #include <util/delay.h>
 
-volatile uint8_t flag = 0;
-volatile uint8_t tick = 0;
-//***********************************************************************
-//                     ISR for timer counter zero
-//***********************************************************************
-ISR(TIMER0_OVF_vect) 
-{ 
-    flag = 1; 
-    tick++;
-}
+uint16_t cnt = 0;
 
 //*******************************************************************************
-//                            debounce_switch                                  
+//                                                          debounce_switch                                  
 // Check pushbuttons on PORTA. 
 // Returns which button was pressed 0-7.
 //*******************************************************************************
 uint8_t debounce_switch() 
 {
-    DDRA = 0x00; // DDRA to input
-    PORTB |= (7<<4); // Enable button board
+    uint8_t save_a = DDRA;
+
+    // DDRA to input
+    DDRA = 0x00;
+    // Enable button board 
+    PORTB |= (7<<4); 
     
-    static uint16_t SR[8] = {0,0,0,0,0,0,0,0}; // Button press shift register
+    // Button press shift register
+    static uint16_t SR[8] = {0,0,0,0,0,0,0,0}; 
     uint8_t i = 0;
     uint8_t ret_val = 9;
 
@@ -41,12 +37,13 @@ uint8_t debounce_switch()
         if (SR[i] == 0x000F) { ret_val = i; }
     }
 
-    DDRA = 0xFF; // set PORTA for output
+
+    DDRA = save_a; 
     return ret_val;
 }
 
 //*******************************************************************************
-//                           to_digs 
+//                                                          to_digs 
 // Returns an array pointer
 // The array is a digit wise separation of numbers 0 to 3.
 // For example, passing num as 1234 will result in digs[0] = 4, digs[1] = 2, etc...
@@ -74,7 +71,7 @@ uint8_t * to_digs(uint16_t num)
 }
 
 //*******************************************************************************
-//                          set_disp 
+//                                                          set_disp 
 // Set which digit is displayed. 
 //*******************************************************************************
 void set_disp(uint8_t disp)
@@ -88,10 +85,10 @@ void set_disp(uint8_t disp)
 }
 
 //*******************************************************************************
-//                          read_spi()
+//                                                          read_spi
 // Reads encoder and writes to bar graph
-    // PIN E 7 is sh/ld which is active low to load
-    // PIN E 6 is clk_inhibit which inhibits the clock when held high
+// PIN E 7 is sh/ld which is active low to load
+// PIN E 6 is clk_inhibit which inhibits the clock when held high
 //*******************************************************************************
 uint8_t spi_read() 
 {
@@ -106,36 +103,100 @@ uint8_t spi_read()
     return temp;
 }
 
+//***********************************************************************
+//                                                          ISR
+//
+// Executes on TCNTR0 overflow.
+// Checks push buttons.
+// Checks the rotary encoders via SPI.
+// Writes to the bar graph via SPI.
+// Increments or decrements cnt variable.
+//***********************************************************************
+ISR(TIMER0_OVF_vect) 
+{ 
+    static uint8_t multiplier = 1;
+    static uint8_t ec_state = 0;
+    uint8_t temp = 0;
+    uint8_t ec1_curr = 0;
+    uint8_t ec1_prev = 0;
+    uint8_t ec2_curr = 0;
+    uint8_t ec2_prev = 0;
+
+    // Check buttons
+    temp = debounce_switch();
+    if (temp<8) { multiplier = temp; }
+    
+    ec_state = (ec_state << 4);
+    ec_state |= ~spi_read();
+    ec1_prev = (ec_state & 0b00110000)>>4;
+    ec1_curr =  ec_state & 0b00000011;
+    ec2_prev = (ec_state & 0b11000000)>>6;
+    ec2_curr = (ec_state & 0b00001100)>>2 ;
+    if (ec1_prev != ec1_curr) 
+    {
+        if ( (ec1_prev == 0b11) && (ec1_curr == 0b01) ) 
+        {   
+            if (multiplier + cnt >= 1023) { cnt = 1023; } 
+            else { cnt += multiplier; }
+        }
+        else if ( (ec1_prev == 0b11) && (ec1_curr == 0b10) ) 
+        {   
+            if (multiplier >= cnt) { cnt = 0; } 
+            else { cnt -= multiplier; }
+        }
+    }
+    if (ec2_prev != ec2_curr) 
+    {
+        if ( (ec2_prev == 0b11) && (ec2_curr == 0b01) ) 
+        {   
+            if (multiplier + cnt >= 1023) { cnt = 1023; } 
+            else { cnt += multiplier; }
+        }
+        else if ( (ec2_prev == 0b11) && (ec2_curr == 0b10) ) 
+        {   
+            if (multiplier >= cnt) { cnt = 0; } 
+            else { cnt -= multiplier; }
+        }
+    }
+}
+
 //*******************************************************************************
-//                            main 
+//                                                          spi_setup
+//
+//*******************************************************************************
+void spi_setup()
+{
+    SPCR |= (1 << SPE) | (1 << MSTR); // SPI ctr reg -- SPI enable, MSTR
+    SPSR |= (1 << SPI2X); // SPI status reg -- set clk/2
+}
+
+//*******************************************************************************
+//                                                          tcntr_setup
+//
+//*******************************************************************************
+void tcntr_setup()
+{
+    TIMSK |= (1 << TOIE0); // Enable interrupts
+    TCCR0 |= (1 << CS02) | (1 << CS00); // Normal mode, prescale by 128
+}
+
+//*******************************************************************************
+//                                                          main 
 // Check active low switches on PORTB.  
 // If low for 4 passes of debounc_switch() increment counter.
 // Display number on all four digits of the LED display board.
 //*******************************************************************************
 int main()
 {
-    uint16_t cnt = 0;
     uint8_t state = 0;
-    uint8_t ec_state = 0;
-    uint8_t ec1_curr = 0;
-    uint8_t ec1_prev = 0;
-    uint8_t ec2_curr = 0;
-    uint8_t ec2_prev = 0;
-    uint8_t i = 0;
 
     DDRB = 0b11110111; // Set to output except input on spi pin 3
     DDRE = 0xFF; // Set to output
     DDRA = 0xFF; // Set to output
 
     sei();
-
-    // Setup Timer 0 with interrupts enabled
-    TIMSK |= (1 << TOIE0); // Enable interrupts
-    TCCR0 |= (1 << CS02) | (1 << CS00); // Normal mode, prescale by 128
-
-    // Setup SPI
-    SPCR |= (1 << SPE) | (1 << MSTR); // SPI ctr reg -- SPI enable, MSTR
-    SPSR |= (1 << SPI2X); // SPI status reg -- set clk/2
+    tcntr_setup();
+    spi_setup();
     
     while(1) // Loop forever
     { 
@@ -148,26 +209,6 @@ int main()
         PORTA = to_digs(cnt)[state];
         _delay_us(15); // delay with the leds on
         PORTA = 0xFF; // turn leds off
-        
-
-        if (flag) // If ISR sets flag
-        {
-            flag = 0;
-
-            // Check buttons
-            if (debounce_switch()<8) { cnt++; }
-
-            ec_state = (ec_state << 4);
-            ec_state |= ~spi_read();
-            ec1_prev = ((ec_state & 0b00110000)>>4);
-            ec1_curr = (ec_state & 0b00000011);
-            if (ec1_prev != ec1_curr) 
-            {
-                if ((ec1_prev == 0b11) && (ec1_curr == 0b01)) { cnt++; }
-                else if((ec1_prev == 0b11) && (ec1_curr == 0b10)) { cnt--; }
-            }
-            
-        }
     }  
 } 
 
